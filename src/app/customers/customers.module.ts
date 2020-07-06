80a89a39e569ef4dfa21d21bf98c2c74a7413d3e
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-07-06 16:15:04 +0700

    Data Binding — Working with Pipes

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../shared/shared.module';
 import { CustomersComponent } from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list-component';
 import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
@@ -11,7 +12,7 @@ import { FilterTextboxComponent } from './customers-list/filter-textbox.componen
     CustomersListComponent,
     FilterTextboxComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   providers: [],
   bootstrap: [CustomersComponent],
   exports: [CustomersComponent], // use exports for using component after export as module
