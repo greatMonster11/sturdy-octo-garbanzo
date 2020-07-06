3d01fd72137b8a51d64efcc28fa88e1569771a33
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-07-06 15:24:54 +0700

    Data Binding — Input Properties

--- src/app/customers/customers-list/customers-list-component.ts
+++ src/app/customers/customers-list/customers-list-component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ICustomer } from '../../shared/interfaces';
 
 @Component({
@@ -6,49 +6,34 @@ import { ICustomer } from '../../shared/interfaces';
   templateUrl: './customers-list.component.html',
 })
 export class CustomersListComponent implements OnInit {
+  private _customers: ICustomer[] = [];
+  @Input() get customers(): ICustomer[] {
+    return this._customers;
+  }
+
+  set customers(value: ICustomer[]) {
+    if (value) {
+      this.filteredCustomers = this._customers = value;
+      this.calculateOrders();
+    }
+  }
+
   filteredCustomers: ICustomer[] = [];
   customersOrderTotal: number;
   currencyCode: string = 'USD';
 
   constructor() {}
 
-  ngOnInit() {
-    this.filteredCustomers = [
-      {
-        id: 1,
-        name: 'john Doe',
-        city: 'Phoenix',
-        orderTotal: 9.99,
-        customerSince: new Date(2014, 7, 10),
-      },
-      {
-        id: 2,
-        name: 'Jane Doe',
-        city: 'Chandler',
-        orderTotal: 19.99,
-        customerSince: new Date(2017, 2, 22),
-      },
-      {
-        id: 3,
-        name: 'Michelle Thomas',
-        city: 'Seattle',
-        orderTotal: 99.99,
-        customerSince: new Date(2002, 10, 31),
-      },
-      {
-        id: 4,
-        name: 'Jim Thomas',
-        city: 'New York',
-        orderTotal: 599.99,
-        customerSince: new Date(2002, 10, 31),
-      },
-    ];
-  }
+  ngOnInit() {}
 
   calculateOrders() {
     this.customersOrderTotal = 0;
     this.filteredCustomers.forEach((cust: ICustomer) => {
       this.customersOrderTotal += cust.orderTotal;
     });
+  }
+
+  sort(prop: string) {
+    // A Handler service will handle the sorting
   }
 }
