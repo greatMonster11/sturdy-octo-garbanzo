57f348f222805302139eacccad2f6673bc701bce
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-07-07 09:42:52 +0700

    Services and Http — Subscribing to an Observable

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { DataService } from '../core/data.service';
+import { ICustomer } from '../shared/interfaces';
 
 @Component({
   selector: 'app-customers',
@@ -15,6 +16,9 @@ export class CustomersComponent implements OnInit {
 
   ngOnInit() {
     this.title = 'Customers';
+    this.dataService
+      .getCustomers()
+      .subscribe((customers: ICustomer[]) => (this.people = customers));
     // this.people = [
     //   {
     //     id: 1,
