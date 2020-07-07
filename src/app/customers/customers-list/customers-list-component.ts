e1649a65be3301e6cca5e7518fba65375c0edf9f
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-07-07 09:53:18 +0700

    Services and Http — Using a SorterService

--- src/app/customers/customers-list/customers-list-component.ts
+++ src/app/customers/customers-list/customers-list-component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ICustomer } from '../../shared/interfaces';
+import { SorterService } from '../../core/sorter.service';
 
 @Component({
   selector: 'app-customers-list',
@@ -22,7 +23,7 @@ export class CustomersListComponent implements OnInit {
   customersOrderTotal: number;
   currencyCode: string = 'USD';
 
-  constructor() {}
+  constructor(private sorterService: SorterService) {}
 
   ngOnInit() {}
 
@@ -49,6 +50,6 @@ export class CustomersListComponent implements OnInit {
   }
 
   sort(prop: string) {
-    // A Handler service will handle the sorting
+    this.sorterService.sort(this.filteredCustomers, prop);
   }
 }
