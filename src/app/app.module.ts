c5e5b4db5ac6216b0dd39d7a9627f05dea769565
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-07-07 10:03:42 +0700

    Routing — Creating a Routing Module with Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { CustomersModule } from './customers/customers.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, CustomersModule, SharedModule],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    CustomersModule,
+    SharedModule,
+    AppRoutingModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
